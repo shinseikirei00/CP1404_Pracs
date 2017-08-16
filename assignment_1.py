bb0ebe0e41268e651975a2c5e7270e642e497457
shinseikirei00 <30421369+shinseikirei00@users.noreply.github.com>
2017-08-17 09:29:21 +1000

    start assignment 1

--- assignment_1.py
+++ assignment_1.py
@@ -4,7 +4,10 @@ Replace the contents of this module docstring with your own details.
 
 
 def main():
-    print("Songs To Learn 1.0 - by <Your Name>")
+    print("Songs To Learn 1.0 - by <Jan Crooks>")
+    menu_entry = input("Menu:\nL - List Songs\nC - Complete Songs\nQ - Quit\n>>>")
+
+
 
 
 if __name__ == '__main__':
