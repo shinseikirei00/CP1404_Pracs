39a2987f50101daec53fee288a5325aeb1d894b2
shinseikirei00 <30421369+shinseikirei00@users.noreply.github.com>
2017-08-17 09:14:22 +1000

    start assignment 1

--- assignment_1.py
+++ assignment_1.py
@@ -1,4 +1,12 @@
-"""Assignment 1 for CP1404 2017
-created by: Jan Crooks
 """
+Replace the contents of this module docstring with your own details.
+"""
+
+
+def main():
+    print("Songs To Learn 1.0 - by <Your Name>")
+
+
+if __name__ == '__main__':
+    main()
 
