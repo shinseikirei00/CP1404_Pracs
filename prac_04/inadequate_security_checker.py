bb1638588ce0346d506b81d5c0dd18647d67c714
shinseikirei00 <30421369+shinseikirei00@users.noreply.github.com>
2017-08-24 09:30:47 +1000

    completed list comprehension

---
+++ prac_04/inadequate_security_checker.py
@@ -0,0 +1,12 @@
+# Woefully inadequate security checker
+# by Jan Crooks
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole',
+             'InterpreterInterface', 'StartServer', 'bob']
+
+user_input = input("Please enter your username: ")
+if user_input in usernames:
+    print("Access Granted")
+
+else:
+    print("Access Denied")
+
